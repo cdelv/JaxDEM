ecdc96cb76f92f9164c638307d12b495da8e67e7
Carlos del Valle <cdelv@unal.edu.co>
2025-02-19 11:12:51 -0500

    license headers

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2025, Carlos Andres del Valle
+#
+# This file is part of the JaxDEM library. For more information and source code
+# availability visit https://github.com/cdelv/JaxDEM
+#
+# JaxDEM is free software; you can redistribute it and/or modify it under the
+# terms of the BSD-3 license. We welcome feedback and contributions
+
 from setuptools import setup
 
 setup(
