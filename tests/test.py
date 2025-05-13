2af2104acbadbebacb644554a8a76083e3b9ea3a
cdelv <cdelv@unal.edu.co>
2025-05-12 20:54:58 -0400

    Delete everything to start a rewrite

--- tests/test.py
+++
@@ -1,5 +0,0 @@
-# This file is part of the JaxDEM library. For more information and source code
-# availability visit https://github.com/cdelv/JaxDEM
-#
-# JaxDEM is free software; you can redistribute it and/or modify it under the
-# terms of the BSD-3 license. We welcome feedback and contributions
