c48c25e694d425747de4398ba00f4cc342471f98
cdelv <cdelv@unal.edu.co>
2025-07-31 19:49:53 -0400

    change theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,15 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import pathlib, sys, datetime
 root = pathlib.Path(__file__).resolve().parents[2]
-sys.path.insert(0, str(root))          # import jaxdem from repo
-
-html_theme = "furo"
-
-html_theme_options = {
-    "light_logo": "logo-light.svg",
-    "dark_logo": "logo-dark.svg",
-    "sidebar_hide_name": True,
-}
+sys.path.insert(0, str(root))      # import jaxdem from repo
 
 project = 'JaxDEM'
 author = 'Carlos Andres del Valle'
@@ -26,22 +18,44 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary',
-    'myst_parser',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "myst_parser",
 ]
 
 autosummary_generate = True
 autodoc_typehints    = "description"
 
+html_theme = "pydata_sphinx_theme"
+
+html_theme_options = {
+    "logo": {
+        "image_light": "logo-light.svg",
+        "image_dark":  "logo-dark.svg",
+    },
+    "navbar_start": ["navbar-logo"],
+    "navbar_center": ["navbar-nav"],
+    "navbar_end": ["theme-switcher", "navbar-icon-links", "search-field"],
+    "show_toc_level": 2,                  # numbered headings depth
+    "collapse_navigation": True,          # ← collapsible sidebar
+
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/cdelv/JaxDEM",
+            "icon": "fab fa-github",
+            "type": "fontawesome",
+        },
+    ],
+}
+
+# narrower content column (optional)
+html_css_files = ["custom.css"]           # see step-3
+
+html_static_path = ["_static"]
+
 templates_path = ['_templates']
 exclude_patterns = []
 
 language = 'en'
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
