5b08b86ce5f1ec35551744332df35547352803e7
cdelv <cdelv@unal.edu.co>
2025-07-31 19:23:07 -0400

    expose all base classes to user

--- jaxdem/__init__.py
+++ jaxdem/__init__.py
@@ -3,10 +3,17 @@
 
 from .State import State
 from .System import System
-from .IO import VTKWriter
+from .IO import VTKWriter, VTKBaseWriter
 from .Materials import Material, MaterialTable 
+from .MaterialMatchmaker import MaterialMatchmaker
 from .Forces import ForceModel
+from .Integrator import Integrator
+from .Collider import Collider
+from .Domain import Domain
+from .Factory import Factory
+from .ForceRouter import ForceRouter, LawCombiner
 
 __all__ = [
-    "State", "System", "VTKWriter", "Material", "MaterialTable", "ForceModel"
+    "State", "System", "VTKWriter", "VTKBaseWriter", "Material", "MaterialTable", "MaterialMatchmaker", "ForceModel",
+    "Integrator", "Collider", "Domain", "Factory", "ForceRouter", "LawCombiner"
 ]
