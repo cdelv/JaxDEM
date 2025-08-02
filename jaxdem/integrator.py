0d53e6a5cf2f9d111206d9c3e43d9ca042529cb3
cdelv <cdelv@unal.edu.co>
2025-08-02 11:59:24 -0400

    Documentation of integrator

--- jaxdem/integrator.py
+++ jaxdem/integrator.py
@@ -12,30 +12,32 @@ from typing import Tuple
 
 from .factory import Factory
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from .state import State
     from .system import System
-    
+
+
 @jax.tree_util.register_dataclass
 @dataclass(slots=True)
 class Integrator(Factory["Integrator"], ABC):
     """
-    This class serves as a factory and provides a standard interface for time-stepping. 
-    Subclasses must implement the `step` method to define how particle states are updated over time.
-
-    Attributes
-    ----------
-    None (interface class)
-
-    Methods
-    -------
-    step(state: State, system: System) -> Tuple[State, System]
-        Abstract method to be implemented by subclasses, defining how to advance the simulation state by one time step.
+    Abstract base class for defining the interface for time-stepping.
 
     Notes
     -----
     - Must Support 2D and 3D domains.
     - Must be jit compatible
+
+    Example
+    -------
+    To define a custom integrator, inherit from :class:`Integrator` and implement its abstract methods:
+
+    >>> @Integrator.register("myCustomIntegrator")
+    >>> @jax.tree_util.register_dataclass
+    >>> @dataclass(slots=True)
+    >>> class MyCustomIntegrator(Integrator):
+            ...
     """
 
     @staticmethod
@@ -43,28 +45,29 @@ class Integrator(Factory["Integrator"], ABC):
     @jax.jit
     def step(state: "State", system: "System") -> Tuple["State", "System"]:
         """
-        Advance the simulation state by one time step using a specific 
-        numerical integration method.
+        Advance the simulation state by one time step using a specific numerical integration method.
 
         Parameters
         ----------
         state : State
+            Current state of the simulation.
         system : System
+            Simulation system configuration.
 
         Returns
         -------
         Tuple[State, System]
-            A tuple containing the updated State and System after one 
-            time step of integration.
+            A tuple containing the updated State and System after one time step of integration.
 
         Raises
         ------
         NotImplementedError
-            If the method is not implemented by a subclass.
+            This is an abstract method and must be implemented by subclasses.
 
-        Notes
-        -----
-        This method must be compatible with jax.jit.
+        Example
+        -------
+
+        >>> state, system = system.integrator.step(state, system)
         """
         raise NotImplementedError
 
@@ -73,58 +76,81 @@ class Integrator(Factory["Integrator"], ABC):
     @jax.jit
     def initialize(state: "State", system: "System") -> Tuple["State", "System"]:
         """
-        Some integration methods require an initialization step, like Leap Frog. 
-        This function implements the interface for initialization.
+        Some integration methods require an initialization step, for example LeapFrog.
+        This function implements the interface for the initialization.
 
         Parameters
         ----------
         state : State
+            Current state of the simulation.
         system : System
+            Simulation system configuration.
 
         Returns
         -------
         Tuple[State, System]
-            A tuple containing the updated State and System 
+            A tuple containing the updated State and System after the initialization.
 
         Raises
         ------
         NotImplementedError
-            If the method is not implemented by a subclass.
+            This is an abstract method and must be implemented by subclasses.
 
-        Notes
-        -----
-            This method must be compatible with jax.jit.
+        Example
+        -------
+
+        >>> state, system = system.integrator.initialize(state, system)
         """
         raise NotImplementedError
+
 
 @Integrator.register("euler")
 @jax.tree_util.register_dataclass
 @dataclass(slots=True)
 class DirectEuler(Integrator):
     """
-    Direct-Euler integration method.
+    Implements the explicit (forward) Euler integration method.
+
+    Notes
+    -----
+    - This method performs the following updates:
+        1.  Applies boundary conditions using :meth:`system.domain.shift`.
+        2.  Computes forces and accelerations using :meth:`system.collider.compute_force`.
+        3.  Updates velocities based on current acceleration.
+        4.  Updates positions based on the newly updated velocities.
+    - Particles with `state.fixed` set to `True` will have their velocities
+      and positions unaffected by the integration step.
     """
+
     @staticmethod
     @jax.jit
     def step(state: "State", system: "System") -> Tuple["State", "System"]:
         """
-        Direct-Euler integration method.
+        Advances the simulation state by one time step using the Direct Euler method.
+
+        The update equations are:
+
+        .. math::
+            & v(t + \\Delta t) &= v(t) + \\Delta t a(t) \\\\
+            & r(t + \\Delta t) &= r(t) + \\Delta t v(t + \\Delta t)
+
+        where:
+            - :math:`r` is the particle position (:attr:`state.pos`)
+            - :math:`v` is the particle velocity (:attr:`state.vel`)
+            - :math:`a` is the particle acceleration (:attr:`state.accel`)
+            - :math:`\\Delta t` is the time step (:attr:`system.dt`)
 
         Parameters
         ----------
         state : State
+            Current state of the simulation.
         system : System
+            Simulation system configuration.
 
         Returns
         -------
         Tuple[State, System]
             The updated state and system after one time step.
-
-        Notes
-        -----
-        Direct Euler:
-        - Increments velocity: v(t+dt) = v(t) + a(t) * dt
-        - Updates position: x(t+dt) = x(t) + v(t+dt) * dt
         """
         state, system = system.domain.shift(state, system)
         state, system = system.collider.compute_force(state, system)
@@ -136,16 +162,19 @@ class DirectEuler(Integrator):
     @jax.jit
     def initialize(state: "State", system: "System") -> Tuple["State", "System"]:
         """
-        Direct-Euler does not need initialization method.
+        The Direct Euler integrator does not require a specific initialization step.
+
 
         Parameters
         ----------
         state : State
+            Current state of the simulation.
         system : System
+            Simulation system configuration.
 
         Returns
         -------
         Tuple[State, System]
-            The updated state and system
+            The original `State` and `System` objects.
         """
         return state, system
