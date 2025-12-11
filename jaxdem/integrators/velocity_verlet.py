34affe08c478f90bb21b0947437f6151041d783d
marshall <mmccraw98@gmail.com>
2025-12-11 12:32:48 -0500

    adding 2d velocity verlet

--- jaxdem/integrators/velocity_verlet.py
+++ jaxdem/integrators/velocity_verlet.py
@@ -10,7 +10,7 @@ from dataclasses import dataclass
 from functools import partial
 from typing import TYPE_CHECKING, Tuple
 
-from . import LinearIntegrator
+from . import LinearIntegrator, RotationIntegrator
 
 if TYPE_CHECKING:  # pragma: no cover
     from ..state import State
@@ -102,5 +102,93 @@ class VelocityVerlet(LinearIntegrator):
         state.vel += accel * (1 - state.fixed)[..., None] * system.dt / 2
         return state, system
 
+@RotationIntegrator.register("rot2dverlet")
+@jax.tree_util.register_dataclass
+@dataclass(slots=True)
+class RotationVelocityVerlet(RotationIntegrator):
+    """
+    Implements the Velocity Verlet integration method for rotations in 2d.
+    """
 
-__all__ = ["VelocityVerlet"]
+    @staticmethod
+    @partial(jax.jit, donate_argnames=("state", "system"), inline=True)
+    @partial(jax.named_call, name="RotationVelocityVerlet.step_after_force")
+    def step_before_force(state: "State", system: "System") -> Tuple["State", "System"]:
+        """
+        Advances the simulation state by one half-step before the force calculation using the Velocity Verlet scheme for rotations.
+
+        The update equations are:
+
+        .. math::
+            & \\omega(t + \\Delta t / 2) &= \\omega(t) + \\Delta t \tau(t) / 2 I \\\\
+            & \\theta(t + \\Delta t) &= \\theta(t) + \\Delta t \\omega(t + \\Delta t / 2) \\Delta t
+
+        where:
+            - :math:`\\theta` is the particle angle in 2d (:attr:`jaxdem.State.q.w`)
+            - :math:`\\omega` is the particle angular velocity in 2d (:attr:`jaxdem.State.angVel`)
+            - :math:`\\tau` is the particle torque computed from forces (:attr:`jaxdem.State.torque`)
+            - :math:`I` is the particle inertia (:attr:`jaxdem.State.inertia`)
+            - :math:`\\Delta t` is the time step (:attr:`jaxdem.System.dt`)
+
+        Parameters
+        ----------
+        state : State
+            Current state of the simulation.
+        system : System
+            Simulation system configuration.
+
+        Returns
+        -------
+        Tuple[State, System]
+            The updated state and system after one time step.
+
+        Note
+        -----
+        - This method donates state and system
+        """
+        if state.dim != 2:
+            raise ValueError(f'Verlet integration only works for state.dim==2, not {state.dim}')
+        accel = state.torque / state.inertia
+        state.angVel += accel * (1 - state.fixed)[..., None] * system.dt / 2
+        state.q.w += state.angVel * (1 - state.fixed)[..., None] * system.dt
+        return state, system
+
+    @staticmethod
+    @partial(jax.jit, donate_argnames=("state", "system"), inline=True)
+    @partial(jax.named_call, name="VelocityVerlet.step_after_force")
+    def step_after_force(state: "State", system: "System") -> Tuple["State", "System"]:
+        """
+        Advances the simulation state by one half-step after the force calculation using the Velocity Verlet scheme for the rotations.
+
+        The update equations are:
+
+        .. math::
+            & \\omega(t + \\Delta t) &= \\omega(t + \\Delta t / 2) + \\Delta t \\tau(t) / 2  I\\\\
+
+        where:
+            - :math:`\\omega` is the particle angular velocity (:attr:`jaxdem.State.angVel`)
+            - :math:`\\tau` is the particle torque computed from forces (:attr:`jaxdem.State.torque`)
+            - :math:`I` is the particle inertia (:attr:`jaxdem.State.inertia`)
+            - :math:`\\Delta t` is the time step (:attr:`jaxdem.System.dt`)
+
+        Parameters
+        ----------
+        state : State
+            Current state of the simulation.
+        system : System
+            Simulation system configuration.
+
+        Returns
+        -------
+        Tuple[State, System]
+            The updated state and system after one time step.
+
+        Note
+        -----
+        - This method donates state and system
+        """
+        accel = state.torque / state.inertia
+        state.angVel += accel * (1 - state.fixed)[..., None] * system.dt / 2
+        return state, system
+
+__all__ = ["VelocityVerlet", "RotationVelocityVerlet"]
